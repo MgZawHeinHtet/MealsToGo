1eda2b6713e5c2257664fad0ed46fd0e938e751f
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2024-10-09 08:57:19 +0630

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app nice!</Text>
+        <Text>This is very nice technigque to do somthing greate</Text>
       <StatusBar style="auto" />
     </View>
   );
